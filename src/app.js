188317c6e6f47cbf5cd477e3e209639c8dbd2952
erik-marlon <erik1903marlon@gmail.com>
2025-12-16 15:00:25 -0300

    Páginas
    Realizado a criação de uma página individual para cada cálculo.

--- src/app.js
+++ src/app.js
@@ -1,21 +1,37 @@
-function calcularPot() {
+function calcularPotconsumida() {
         var potnb = Number(document.querySelector('#potnb').value)
         var Pcargasaida = Number(document.querySelector('#Pcargasaida').value) / 100
         var FPsaida = Number(document.querySelector('#FPsaida').value)
         var inversor = Number(document.querySelector('#inversor').value) / 100
+
+        var Potconsumida = (potnb * Pcargasaida * FPsaida) / inversor
+
+        document.querySelector('#valorRes1').innerText = Potconsumida.toFixed(2) + " W"
+}
+
+function calcularTensaobat() {
         var Qbaterias = Number(document.querySelector('#Qbaterias').value)
+
+        var Tensaobat = Qbaterias * 12
+        
+        document.querySelector('#valorRes2').innerText = Tensaobat + " V"
+}
+
+function calcularCdescarga() {
+        var potconsumida = Number(document.querySelector('#potconsumida').value)
+        var Tensaobat = Number(document.querySelector('#Tensaobat').value)
+
+        var Cdescarga = (potconsumida * 1000) / Tensaobat
+
+        document.querySelector('#valorRes3').innerText = Cdescarga.toFixed(2) + " A"
+}
+
+function calcularAutonomia() {
+        var Cdescarga = Number(document.querySelector('#Cdescarga').value)
         var correnteCAT = Number(document.querySelector('#correnteCAT').value)                      
         var tempoCAT = Number(document.querySelector('#tempoCAT').value)
 
-
-        var Tensaobat = Qbaterias * 12
-        var Potconsumida = (potnb * Pcargasaida * FPsaida) / inversor
-        var Cdescarga = (Potconsumida * 1000) / Tensaobat
         var minutoscorrente = (correnteCAT / Cdescarga) * tempoCAT
         
-        document.querySelector('#valorRes1').innerText = Potconsumida.toFixed(2) + " W"
-        document.querySelector('#valorRes2').innerText = Tensaobat + " V"
-        document.querySelector('#valorRes3').innerText = Cdescarga.toFixed(2) + " A"
         document.querySelector('#valorRes4').innerText = minutoscorrente.toFixed(2)
-
 }
